b1ddcc5c2507bdcb4b88c5aed9baf3f932a79ff6
marcelbuesing <buesing.marcel@googlemail.com>
2016-09-25 21:16:08 +0200

    Cleanup code

--- src/Main.hs
+++ src/Main.hs
@@ -26,7 +26,9 @@ main :: IO ()
 main = do
   s <- initCan "vcan0"
   canSend s
-  forever $ canRead s
+  forever $ do
+    cf <- canRead s
+    print $ show cf
   close s
   return ()
 
@@ -42,8 +44,8 @@ canRaw = read "1" :: ProtocolNumber
 initCan :: String ->  IO Socket
 initCan addr = do
   s <- socket AF_CAN Raw canRaw
-  idx <-  liftM fromJust $ ifNameToIndex addr
-  _ <- bind s (SockAddrCan $ fromIntegral $ idx)
+  idx <-  fromJust <$> ifNameToIndex addr
+  bind s (SockAddrCan $ fromIntegral idx)
   return s
               where idx = fromJust <$> ifNameToIndex addr
 
@@ -56,17 +58,13 @@ canTestMsg = S.pack [ 0,0,0,0 -- can ID = 0
 canSend :: Socket -> IO Int
 canSend s = send s canTestMsg
 
-canRead :: Socket -> IO Int
-canRead s = do
-  --buf <- malloc :: IO (Ptr CanFrame)
+canRead :: Socket -> IO (Either String CanFrame)
+canRead s =
   alloca $ \ptrCf -> do
-    (cnt, sa) <- recvBufFrom s (ptrCf :: Ptr CanFrame) 8
+    (cnt, _) <- recvBufFrom s (ptrCf :: Ptr CanFrame) 8
     if cnt > 0 then do
-      cf <-(peek ptrCf) :: IO CanFrame
-      _ <- (print . show) cf
-      return cnt
-    else do
-      print "Length of buffer is zero"
-      return cnt
+      frame <- peek ptrCf
+      return $ Right frame
+    else return $ Left "Buffer is empty"
 
 
